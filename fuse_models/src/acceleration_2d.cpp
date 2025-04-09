0352541400bfbf8eec08421254f52727fe6af38e
Dan Murphy <dan.murphy101@gmail.com>
2025-04-09 15:13:15 -0400

    Only instantiate tf_listeners if needed
    - We find that the transform listeners in sensor models are negatively
      impacting performance
    - Do not instantiate transform listeners if target frames are empty
    - Target frames are empty by default
    - Existing logic already does not do the transforms when target frames
      are empty

--- fuse_models/src/acceleration_2d.cpp
+++ fuse_models/src/acceleration_2d.cpp
@@ -51,7 +51,6 @@ namespace fuse_models
 Acceleration2D::Acceleration2D() :
   fuse_core::AsyncSensorModel(1),
   device_id_(fuse_core::uuid::NIL),
-  tf_listener_(tf_buffer_),
   throttled_callback_(std::bind(&Acceleration2D::process, this, std::placeholders::_1))
 {
 }
@@ -70,6 +69,11 @@ void Acceleration2D::onInit()
   {
     ROS_WARN_STREAM("No dimensions were specified. Data from topic " << ros::names::resolve(params_.topic) <<
                     " will be ignored.");
+  }
+
+  if (!params_.target_frame.empty())
+  {
+    tf_listener_ = std::make_unique<tf2_ros::TransformListener>(tf_buffer_);
   }
 }
 
